249369667bbdb878b709434010c6dd21bf5769d6
Imran01000 <imshaik01000@gmail.com>
2020-02-25 12:57:58 +0530

    ADDED:To check wheather the employee is present or abscent.

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
-#To displaying welcome to employee.
-echo "welcome employees ";
+#To check wheather the employee is present or abscent.
+#variables.
+isPresent=1
+attendence=$(($RANDOM%2+1))
+if (($isPresent==$attendence))
+then
+	echo "Employee is present";
+else
+	echo "Employee is abscent";
+fi
+
