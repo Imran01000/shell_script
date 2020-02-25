09183c560f37cdd0357d37b787ff2c1b376bd439
Imran01000 <imshaik01000@gmail.com>
2020-02-25 14:19:42 +0530

    MODIFIED:Added the previous use case.

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,14 +1,33 @@
 #!/bin/bash -x
-#To calculate daily empolyee wage.
+#To displaying welcome to employee.
+echo "welcome employees ";
+
+#To check wheather the employee is present or abscent.
 #variables.
-isPresent=1;
-attendence=$(($RANDOM%2+1));
+
+isPresent=1
+attendence=$(($RANDOM%2+1))
 if (($isPresent==$attendence))
 then
-	empRatePerHr=10;
-	empWorkHr=2;
-	empWage=$(($empRatePerHr*$empWorkHr));
-	echo "Employee wage is $empWage";
+	echo "Employee is present";
 else
 	echo "Employee is abscent";
 fi
+#To calculate daily empolyee wage and also for part time employee.
+#variables.
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=10;
+attendence=$(($RANDOM%3))
+if (($isFullTime==$attendence))
+then
+	empWorkHr=5;
+elif (($isPartTime==$attendence))
+then
+	empWorkHr=2;
+else
+	empWorkHr=0;
+fi
+salary=$(($empRatePerHr*$empWorkHr));
+echo "The salary for employee is $salary";
